b1c44eda3ac5f3fe47e239bd98e149ccfa5ef874
piglitch <73684837+piglitch@users.noreply.github.com>
2023-06-14 18:14:08 +0530

    ..

--- Arrays/crystalBallProblem.ts
+++ Arrays/crystalBallProblem.ts
@@ -1,16 +1,22 @@
 // Given two crystal balls that will break if dropped from high enough distance, determine the exact spot in which it will break-
 // in the most optimized way.
-let breaks = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1]
+const breaks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,1,1,1,1,1,1,1,1,1,1,1, 1]
 
-const jmpAmount = Math.floor(Math.sqrt(breaks.length));
-let i=jmpAmount
-for (; i < breaks.length; i += jmpAmount) {
-    if (breaks[i]){
-        break;
+function cryst(){
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jmpAmount;
+    for (;i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
     }
+    i -= jmpAmount
+    for (let j=0; j < jmpAmount && i<breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1
 }
-i -= jmpAmount;
+console.log(cryst());
 
-for (let j = 0; j < jmpAmount && i < breaks.length){
-
-}
